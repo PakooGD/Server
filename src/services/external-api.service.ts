869b7b4bcbde20f61de0ec63c8d0fcf80e83ddd9
Pakoo <91836361+PakooGD@users.noreply.github.com>
2025-05-16 22:09:51 +0300

    fix: https

--- src/services/external-api.service.ts
+++ src/services/external-api.service.ts
@@ -1,9 +1,6 @@
 import axios from 'axios';
 import { LoginResponse } from '../types/ITypes';
 
-const https = require('https');
-const agent = new https.Agent({ rejectUnauthorized: false });
-
 export class ExternalApiService {
   static async login(headers:any, phone: string, password: string, icc: string): Promise<LoginResponse> {
     const response = await axios.post(`https://passport.xag.cn/api/account/v1/user/token/login`, {
@@ -12,8 +9,6 @@ export class ExternalApiService {
       icc,
     }, {
       headers: headers,
-      httpsAgent: agent, 
-      timeout: 30000,
     });
     return response.data;
   }
@@ -29,8 +24,6 @@ export class ExternalApiService {
       version
     }, {
       headers: headers,
-      httpsAgent: agent, 
-      timeout: 30000,
     });
     return response.data;
   }
@@ -38,24 +31,18 @@ export class ExternalApiService {
   static async getUserSettings(headers:any): Promise<any> {
     return await axios.post(`https://passport.xag.cn/api/account/v1/common/user/setting/get`, {
       headers: headers,
-      httpsAgent: agent, 
-      timeout: 30000,
     });
   }
 
   static async Route(headers:any,accountKey:any): Promise<any> {
     return await axios.get(`https://passport.xag.cn/api/account/v1/common/user/route?account_key=${accountKey}`, {
       headers: headers,
-      httpsAgent: agent, 
-      timeout: 30000,
     });
   }
 
   static async GetDeviceLists(headers:any): Promise<any> {
     return await axios.get('https://dservice.xa.com/api/equipment/device/lists', {
       headers: headers,
-      httpsAgent: agent, 
-      timeout: 30000,
     });
   }
 
@@ -66,8 +53,6 @@ export class ExternalApiService {
         host: 'dservice.xa.com'
       },
       params,
-      httpsAgent: agent, 
-      timeout: 30000,
     });
   }  
   
@@ -78,8 +63,6 @@ export class ExternalApiService {
         host: 'dservice.xa.com'
       },
       params,
-      httpsAgent: agent, 
-      timeout: 30000,
     });
   }
 
