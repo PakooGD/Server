0b1ca7ad24e4744238e4c153738f15f2b804b084
Pakoo <91836361+PakooGD@users.noreply.github.com>
2025-05-17 15:24:49 +0300

    fix routes

--- src/routes/xag.routes.ts
+++ src/routes/xag.routes.ts
@@ -6,7 +6,6 @@ const router = Router();
 router.post('/account/v1/user/token/login', AuthController.Login);
 router.get('/account/v1/common/user/setting/get', AuthController.Setting);
 router.post('/message/v1/jpush/relation/register', AuthController.Register);
-router.get('/equipment/home/getIotUserSession', AuthController.getIotUserSession);
 router.get('/equipment/device/lists', XagController.getDeviceLists);
 router.get('/equipment/device/searchInfo', XagController.searchInfo);
 router.get('/equipment/device/searchStatus', XagController.searchStatus);
