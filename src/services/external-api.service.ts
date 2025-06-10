16c99d145be31f8972dae0e1d390f79196ad7f00
Pakoo <91836361+PakooGD@users.noreply.github.com>
2025-06-10 16:52:36 +0300

    34343

--- src/services/external-api.service.ts
+++ src/services/external-api.service.ts
@@ -1,19 +1,20 @@
 import axios from 'axios';
+import { Request, Response } from 'express';
 
 export class ExternalApiService {
-  public static sendResponse(res: any, result: any) {
+  public static sendResponse(res: Response, result: any) {
     const status = result.status >= 100 && result.status <= 599 ? result.status : 200;
     res.status(status).json({ ...result, status });
   }
 
-  public static async RedirectPost(req: any, host: string, api: string ): Promise<any> {
+  public static async RedirectPost(req: Request, host: string, api: string ): Promise<any> {
     const params = req.query;
     const headers = req.headers;
     headers['Host'] = host;
   
-    const url = `https://passport.xag.cn/${api}`;
+    const url = `https://${host}/${api}`;
   
-    const response = await axios.post(url, req.body, {
+    const response = await axios.post(url, {...req.body}, {
       headers: {
         ...headers,
       },
@@ -23,12 +24,12 @@ export class ExternalApiService {
     return response.data;
   }
 
-  public static async RedirectGet(req: any, host:any, api:any): Promise<any> {
+  public static async RedirectGet(req: Request, host:any, api:any): Promise<any> {
     const params = req.query;
     const headers = req.headers;
     headers['Host'] = host;
 
-    const url = `https://passport.xag.cn/${api}`;
+    const url = `https://${host}/${api}`;
   
     const response = await axios.get(url, {
       headers: {
