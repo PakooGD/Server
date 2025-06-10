15d9029f704f320119a6bf08a3d44cb6ab69afe8
Pakoo <91836361+PakooGD@users.noreply.github.com>
2025-06-10 15:25:29 +0300

    fix: record controller

--- src/controllers/record.controller.ts
+++ src/controllers/record.controller.ts
@@ -1,8 +1,5 @@
 import { Request, Response } from 'express';
 
-import { User } from '../models/user.model';
-import { Device } from '../models/device.model';
-import { ExternalApiService } from '../services';
 
 export class RecordController {
 
